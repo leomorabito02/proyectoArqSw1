d7f92f2430b82de91904230ecfd5e46df67723a9
leomorabito02 <leonardomorabito02@hotmail.com>
2023-06-06 00:07:38 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Login from './pages/Login.jsx';
 import Footer from './components/Footer.jsx'
 import Register from "./pages/Register.jsx";
+import Home from "./pages/Home.jsx";
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
                       <Route path="/" element={<Login />} />
                       <Route path="/login" element={<Login />} />
                       <Route path="/register" element={<Register />} />
+                       <Route path="/home" element={<Home/>}/>
               </Routes>
               <Footer />
           </Router>
